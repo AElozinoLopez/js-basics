4bb309f8a17da223ca414256c4422fffbeac3ddd
Elozino-Lopez <asaijeelozinolopez@gmail.com>
2022-10-27 00:42:44 +0100

    Program to Determine Academic grade

--- index.js
+++ index.js
@@ -1,14 +1,37 @@
+// Program to Print Grade depending on Scores received
+let marks = 83;
+if (marks > 90) {
+    console.log('AA');
+} else if (marks > 80 && marks <= 90) {
+    console.log('AB');
+} else if (marks > 70 && marks <= 80) {
+    console.log("BB");
+} else if (marks > 60 && marks <= 70) {
+    console.log('BC');
+} else if (marks > 50 && marks <= 60) {
+    console.log('CC');
+} else if (marks > 40 && marks <= 50) {
+    console.log('CD');
+} else if ( marks > 30 && marks <= 40) {
+    console.log('DD');
+} else {
+    console.log('FF');
+}
+
+
+
+
 // Program to greet visitors depending on hour of the day
-let hour = 19;
-if (hour >= 1 && hour < 12) {
-    console.log('Good morning')
-}
-else if (hour >= 12 && hour < 18) {
-    console.log ('Good afternoon')
-}
-else {
-    console.log('Good evening')
-}
+// let hour = 19;
+// if (hour >= 1 && hour < 12) {
+//     console.log('Good morning')
+// }
+// else if (hour >= 12 && hour < 18) {
+//     console.log ('Good afternoon')
+// }
+// else {
+//     console.log('Good evening')
+// }
 
 
 // Program to calculate Simple Interest SI = P * R * T  / 100
