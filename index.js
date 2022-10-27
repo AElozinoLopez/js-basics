1e11a5cbbde5a251721a05c977c91c56ad84b2d2
Elozino-Lopez <asaijeelozinolopez@gmail.com>
2022-10-27 08:44:07 +0100

    Corrected the code structure

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 // For Loop to print out "Hello World" a number of times
-for (let i = 0; i < 1050; i++) {
-    console.log('Hello World')
-}
+// for (let i = 0; i < 1050; i++) {
+//     console.log('Hello World')
+// }
 
 //For Loop to display odd numbers between 0 and 10
-for (let i = 0; i % 2 > 0; i++) {
-    console.log('The odd numbers are ' + );
+for (let i = 0; i < 10; i++) {
+    if (i % 2 !== 0)
+    console.log(i);
 }
 
 // Switch...case statement
