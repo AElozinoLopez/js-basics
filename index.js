ae36abcf7e61a21501809b5f0817f1b4b603bff3
Elozino-Lopez <asaijeelozinolopez@gmail.com>
2022-10-29 07:37:23 +0100

    Tried using ternary operator and made the if...else logic cleaner

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 // EXERCISE - Even and Odd Numbers Exercise
 function showNumbers (limit) {
     for (i = 0; i <= limit; i++) {
-        if (i % 2 === 0) {
+        if (i % 2 === 0) 
             console.log(i, " Even");
-        } else {
+         else 
             console.log(i, "Odd ");
-        }
         
+
+               
     }
 }
 
