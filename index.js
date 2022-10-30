a473c733141d5611dc63efc0456b3c56932a8c37
Elozino-Lopez <asaijeelozinolopez@gmail.com>
2022-10-30 05:15:45 +0100

    Added comments to keep track of solution thoughtline

--- index.js
+++ index.js
@@ -1,13 +1,6 @@
 
-// program to print prime numbers between the two numbers
-
-// take input from the user
-// const lowerNumber = parseInt(prompt('Enter lower number: '));
-// const higherNumber = parseInt(prompt('Enter higher number: '));
-
 // EXERCISES: Prime Numbers
 
-// looping from lowerNumber to higherNumber
 function showPrime (limit) {
     for (let i = 0; i <= limit; i++) {
         let num = 0;
@@ -27,7 +20,9 @@ function showPrime (limit) {
 
 showPrime (20);
 
-// EXERCISES: Prime Numbers
+
+
+// EXERCISES: Prime Numbers  ( My first attempt)
 // function showPrime (limit) {
 //     for (let i = 0; i <= limit; i++) {
 //         if (i % 2 !== 0) 
@@ -37,6 +32,48 @@ showPrime (20);
 // }
 
 // showPrime(20);
+
+// SOURCE TO SOLUTION ABOVE 
+
+// program to print prime numbers between the two numbers
+
+// take input from the user
+// const lowerNumber = parseInt(prompt('Enter lower number: '));
+// const higherNumber = parseInt(prompt('Enter higher number: '));
+
+// console.log(`The prime numbers between ${lowerNumber} and ${higherNumber} are:`);
+
+// // looping from lowerNumber to higherNumber
+// for (let i = lowerNumber; i <= higherNumber; i++) {
+//     let flag = 0;
+
+//     // looping through 2 to user input number
+//     for (let j = 2; j < i; j++) {
+//         if (i % j == 0) {
+//             flag = 1;
+//             break;
+//         }
+//     }
+
+//     // if number greater than 1 and not divisible by other numbers
+//     if (i > 1 && flag == 0) {
+//         console.log(i);
+//     }
+// }
+
+// THE EXPLANATION FOR THE ABOVE LOGIC
+
+// In the above program, the user is prompted to enter lower and higher bound numbers. Then the prime number between those numbers (including the lower and higher bounds, if any) are listed out.
+
+// Two nested for loops are used in the above program.
+
+// The first for loop is used to loop between the numbers provided by the user. In this case, from 2 to 10.
+// A variable flag is set to 0.
+// The second for loop is used to loop between 2 to the number that is stored in i.
+// Inside the second loop, the value of i is divided by each number from 2 to value one less than i (i - 1).
+// While dividing, if any number remainder results in 0, that number is not a prime number. So the variable flag is set to 1.
+// Finally, all the numbers that have a flag 0 (not divisible by other numbers) are printed.
+
 
 
 
