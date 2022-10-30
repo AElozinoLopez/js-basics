fe22403dbb95c2283c7885a20491420018d8db5a
Elozino-Lopez <asaijeelozinolopez@gmail.com>
2022-10-30 04:57:07 +0100

    Updated the logic

--- index.js
+++ index.js
@@ -1,13 +1,47 @@
+
+// program to print prime numbers between the two numbers
+
+// take input from the user
+// const lowerNumber = parseInt(prompt('Enter lower number: '));
+// const higherNumber = parseInt(prompt('Enter higher number: '));
+
 // EXERCISES: Prime Numbers
+
+// looping from lowerNumber to higherNumber
 function showPrime (limit) {
-    for (i = 0; i <= limit; i++) {
-        if (i % 2 !== 0) 
-            console.log (i);
+    for (let i = 0; i <= limit; i++) {
+        let flag = 0;
+    
+        // looping through 2 to user input number
+        for (let j = 2; j < i; j++) {
+            if (i % j == 0) {
+                flag = 1;
+                break;
+            }
         }
     
+        // if number greater than 1 and not divisible by other numbers
+        if (i > 1 && flag == 0) {
+            console.log(i);
+        }
+    }
 }
 
-showPrime(20);
+showPrime (20);
+
+// EXERCISES: Prime Numbers
+// function showPrime (limit) {
+//     for (let i = 0; i <= limit; i++) {
+//         if (i % 2 !== 0) 
+//             console.log (i);
+//         }
+    
+// }
+
+// showPrime(20);
+
+
+
 
 // EXERCISES: Sum of multiples of 3 and 5
 // function sum (limit) {
